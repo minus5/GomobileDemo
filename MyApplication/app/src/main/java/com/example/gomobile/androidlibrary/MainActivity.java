04a0473f5a91937ca0af282db8faec0e32830c29
Jovica Popovic <jpop32@gmail.com>
2017-04-01 16:07:07 +0200

    Implementation of callback mechanism for Go->Java communication

--- MyApplication/app/src/main/java/com/example/gomobile/androidlibrary/MainActivity.java
+++ MyApplication/app/src/main/java/com/example/gomobile/androidlibrary/MainActivity.java
@@ -2,16 +2,23 @@ package com.example.gomobile.androidlibrary;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.CheckedTextView;
 import android.widget.EditText;
 import android.widget.TextView;
 
 import androidlibrary.Androidlibrary;
+import androidlibrary.JavaCallback;
 
 public class MainActivity extends AppCompatActivity
 {
+    final static String TAG = MainActivity.class.getSimpleName();
+
     private EditText viewEdit;
     private TextView viewText;
+    private CheckedTextView checkText;
+    private JavaCallback javaCallback;
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
@@ -21,11 +28,43 @@ public class MainActivity extends AppCompatActivity
 
         viewEdit = (EditText) findViewById(R.id.editText);
         viewText = (TextView) findViewById(R.id.viewText);
+        checkText = (CheckedTextView) findViewById(R.id.checkedTextView);
 
+        javaCallback = new JavaCallback()
+        {
+            @Override
+            public void heartbeat()
+            {
+                runOnUiThread(new Runnable()
+                {
+                    @Override
+                    public void run()
+                    {
+                        checkText.toggle();
+                    }
+                });
+            }
+
+            @Override
+            public void relayMessage(String s, long l)
+            {
+                Log.d(TAG, "Got a message from Go: " + s + ", value " + l);
+            }
+        };
     }
 
     public void talkToGo(View view)
     {
         viewText.setText(Androidlibrary.echo(viewEdit.getText().toString()));
+    }
+
+    public void doRegister(View view)
+    {
+        Androidlibrary.registerCallback(javaCallback);
+    }
+
+    public void doUnregister(View view)
+    {
+        Androidlibrary.unregisterCallback();
     }
 }
--- src/androidlibrary/golib.go
+++ src/androidlibrary/golib.go
@@ -1,5 +1,48 @@
 package androidlibrary
 
+import (
+	"time"
+	"log"
+)
+
+type JavaCallback interface {
+	Heartbeat()
+	RelayMessage(msg string, intval int)
+}
+
+var callback JavaCallback
+var ticker *time.Ticker
+var intval int
+
+func RegisterCallback(c JavaCallback) {
+	if callback != nil {
+		UnregisterCallback()
+	}
+
+	callback = c
+
+	log.Print("Callback registered.")
+
+	ticker = time.NewTicker(500 * time.Millisecond)
+	go func() {
+		for range ticker.C {
+			log.Print("Tick.")
+			callback.RelayMessage("Tick.", intval)
+			callback.Heartbeat()
+			intval++
+		}
+	}()
+}
+
+func UnregisterCallback() {
+	if callback != nil {
+		log.Print("Callback unregistered.")
+		ticker.Stop()
+		ticker = nil
+		callback = nil
+	}
+}
+
 func Echo(in string) string {
 	return "Android said: " + in
 }
